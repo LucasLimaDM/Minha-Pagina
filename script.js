142c02c83e98b7558e4959c84be1c5c798afc27e
LucasLimaDM <lucaslim.dias@gmail.com>
2023-02-20 19:32:18 -0300

    Funcionalidade do botão com abertura de sidebar

---
+++ script.js
@@ -0,0 +1,52 @@
+const sidebar = document.querySelector('#sidebar')
+const containerButtons = document.querySelector('#container-buttons')
+sidebar.innerHTML = containerButtons.innerHTML
+const menu = document.querySelector('#menu')
+const headerButtons = document.querySelectorAll('header nav button')
+const sidebarButtons = document.querySelectorAll('#sidebar button')
+
+for (let i = 0; i < sidebarButtons.length; i++) {
+    sidebarButtons[i].addEventListener('click', function(){scrollNaTela(i)})   
+    headerButtons[i].addEventListener('click', function(){scrollNaTela(i)})   
+}
+
+function fechaSidebar() { 
+
+}
+// ao clicar no menu, fecha ou abre a sidebar
+menu.addEventListener('click', function() {
+    if (sidebar.classList.contains('open')) {
+        
+    } else {
+        sidebar.classList.add('open')
+    }
+})
+
+// fecha a sidebar ao alterar tamanho da janela
+document.body.onresize = function(){
+    sidebar.classList.remove('open')
+}
+function scrollNaTela(btnNum) {
+    let targetOffset = document.querySelector(trataPosition(btnNum)).offsetTop
+    window.scrollTo(0, targetOffset - 110)
+    console.log("🚀 ~ file: script.js:29 ~ scrollNaTela ~ targetOffset", targetOffset)
+}
+
+function trataPosition(number) {
+    switch (number) {
+        case 0:
+            return '#presentation';
+            break;
+        case 1:
+            return '#projetos';
+            break;
+        case 2:
+            return '#informacoes';
+            break;
+        case 3:
+            return '#contato';
+            break;
+        default:
+            break;
+    }
+}
