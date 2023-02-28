35ab2a764de2df5227b72abad01205174bf50959
LucasLimaDM <lucaslim.dias@gmail.com>
2023-02-28 17:09:29 -0300

    Refatoração para deploy

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const sidebar = document.querySelector('#sidebar')
-const containerButtons = document.querySelector('#container-buttons')
-sidebar.innerHTML = containerButtons.innerHTML
-const menu = document.querySelector('#menu')
-const headerButtons = document.querySelectorAll('header nav button')
-const sidebarButtons = document.querySelectorAll('#sidebar button')
+const sidebar = document.querySelector('#sidebar');
+const containerButtons = document.querySelector('#container-buttons');
+sidebar.innerHTML = containerButtons.innerHTML;
+const menu = document.querySelector('#menu');
+const headerButtons = document.querySelectorAll('header nav button');
+const sidebarButtons = document.querySelectorAll('#sidebar button');
 
 for (let i = 0; i < sidebarButtons.length; i++) {
     sidebarButtons[i].addEventListener('click', function(){scrollNaTela(i)})   
@@ -11,26 +11,25 @@ for (let i = 0; i < sidebarButtons.length; i++) {
 }
 
 function fechaSidebar() { 
-    sidebar.classList.remove('open')
+    sidebar.classList.remove('open');
 }
 // ao clicar no menu, fecha ou abre a sidebar
 menu.addEventListener('click', function() {
     if (sidebar.classList.contains('open')) {
-        fechaSidebar()
+        fechaSidebar();
     } else {
-        sidebar.classList.add('open')
+        sidebar.classList.add('open');
     }
 })
 
 // fecha a sidebar ao alterar tamanho da janela
 document.body.onresize = function(){
-    fechaSidebar()
+    fechaSidebar();
 }
 function scrollNaTela(btnNum) {
-    fechaSidebar()
-    let targetOffset = document.querySelector(trataPosition(btnNum)).offsetTop
-    window.scrollTo(0, targetOffset - 110)
-    console.log("🚀 ~ file: script.js:29 ~ scrollNaTela ~ targetOffset", targetOffset)
+    fechaSidebar();
+    let targetOffset = document.querySelector(trataPosition(btnNum)).offsetTop;
+    window.scrollTo(0, targetOffset - 110);
 }
 
 function trataPosition(number) {
